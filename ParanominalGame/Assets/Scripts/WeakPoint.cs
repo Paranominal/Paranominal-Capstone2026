8e46d10b847e4d61dda535c7f2ced2a815940801
ekekalex <ekytoras@gmail.com>
2026-03-06 01:08:47 +1100

    weakpoints have multiple slots for graphics

--- ParanominalGame/Assets/Scripts/WeakPoint.cs
+++ ParanominalGame/Assets/Scripts/WeakPoint.cs
@@ -3,26 +3,30 @@ using UnityEngine;
 public class WeakPoint : MonoBehaviour
 {
     [SerializeField] GameObject[] weakpoints;
-    [SerializeField] private GameObject graphic;
+    [SerializeField] private GameObject[] graphics;
     public WeakPointManager weakPointManager;
     private int currentWeakpoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        foreach (GameObject weakpoint in weakpoints)
-        {
-        }
+        
     }
 
     public void Activate()
     {
         gameObject.GetComponent<SphereCollider>().enabled = true;
+        foreach (GameObject graphic in graphics)
+        {
         graphic.GetComponent<SpriteRenderer>().enabled = true;
+        }
     }
     public void Dectivate()
     {
         gameObject.GetComponent<SphereCollider>().enabled = false;
-        graphic.GetComponent<SpriteRenderer>().enabled = false;
+        foreach (GameObject graphic in graphics)
+        {
+            graphic.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
     
     public void OnHit()
