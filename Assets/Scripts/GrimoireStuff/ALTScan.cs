555df8d0b4dce68c7634f7e3e356f5591f431ae1
lstirling <102574044@student.swin.edu.au>
2026-03-19 11:52:15 +1100

    Moved to InteractionPrototype scene and started animations

--- Assets/Scripts/GrimoireStuff/ALTScan.cs
+++ Assets/Scripts/GrimoireStuff/ALTScan.cs
@@ -12,7 +12,7 @@ public class ALTScan : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        scanAction = InputSystem.actions.FindAction("Grimoire");
+        scanAction = InputSystem.actions.FindAction("Scan");
         collectAction = InputSystem.actions.FindAction("Collect");
         if (grimoire == null)
         {
