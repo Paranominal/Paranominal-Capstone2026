ac162f2c0449d8a16abc8888be22beb05847faf3
Eleanor Slade <163821768+sladeel@users.noreply.github.com>
2026-03-21 14:12:09 +1100

    doors + locking and unlocking them

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Door : MonoBehaviour
+{
+    public bool clockwise;
+    public LayerMask interactable;
+    InputAction collectAction;  // this could be rebound to a different action if you prefer
+    public bool unlocked;   // at the moment the door can theoretically be "locked" open but thats neither here nor there
+    private bool open;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        collectAction = InputSystem.actions.FindAction("Collect");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, interactable))
+        {
+            if (collectAction.WasReleasedThisFrame() && unlocked)
+            {
+                if (clockwise && !open || !clockwise && open)
+                {
+                    transform.Rotate(0, 90, 0);
+                    open = !open;
+                }
+                else
+                {
+                    transform.Rotate(0, -90, 0);
+                    open = !open;
+                }
+            }
+        }
+    }
+
+    public void Unlock()
+    {
+        unlocked = true;
+    }
+}
---
+++ Assets/Scripts/GrimoireStuff/InteractionObject.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionObject : MonoBehaviour
+{
+    public string keyName;
+    public LayerMask interactable;
+    private ALTGrimoire grimoire;
+    InputAction collectAction;  // this could be rebound to a different action if you prefer
+    public Door door;
+
+    void Start()
+    {
+        if (grimoire == null)
+        {
+            grimoire = FindAnyObjectByType<ALTGrimoire>();
+        }
+        collectAction = InputSystem.actions.FindAction("Collect");
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, interactable))
+        {
+            if (grimoire.entries.Count != 0)
+            {
+                if (grimoire.GetCurrentEntry().entryName == keyName && grimoire.GetCurrentEntry().collected && collectAction.WasReleasedThisFrame())
+                {
+                    // down the road i'd like to update this to run through a switch statement based on an enum for multiple types of interaction. for now it just unlocks doors.
+                    door.Unlock();
+                    Debug.Log("The sound of a door unlocking. Woah so immersive.");
+                }
+            }
+
+        }
+    }
+}
