9bc2f05fc9f57b6a86297e3d60fa268626a3c56b
lstirling <102574044@student.swin.edu.au>
2026-05-01 20:36:23 +1000

    tha big commit part 1
    note:
    this does not include the scoring, which i've moved to a fork of enemy-attacks for better continuity
    
    changes:
    - scanning now progresses slowly, with a radial progress (using temp reticle asset)
    - scanning now limited by distance
    - different colours can be assigned for different scanning categories - currently handled in code but will probably move to the inspector if i can figure out a good way how
    - scanning now assigned to tab, grimoire opening now assigned to G
    - moved the lock back to the door in interactionprototype (but that is kinda confusing to me that the lock mechanism is not on the lock object)

--- Assets/Scripts/GrimoireStuff/ALTScannableObject.cs
+++ Assets/Scripts/GrimoireStuff/ALTScannableObject.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+public enum ScanCategory { Object, Enemy, KeyItem }
 
 public class ALTScannableObject : MonoBehaviour
 {
     public ALTGrimoireEntry entry; 
     public Outline outline;
     public bool collectable;
+    public ScanCategory category = ScanCategory.Object;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +15,6 @@ public class ALTScannableObject : MonoBehaviour
         outline.enabled = false;
         outline.OutlineWidth = 5f;
         outline.OutlineMode = Outline.Mode.OutlineVisible;
-        if (collectable)
-        {
-            outline.OutlineColor = Color.red;
-        }
+        ScanModeVisuals.instance.RegisterScannable(this);
     }
-
-    
 }
