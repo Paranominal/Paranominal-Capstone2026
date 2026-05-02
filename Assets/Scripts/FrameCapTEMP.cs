aa6b5da14e58e2695080b08b3fdcc328b22481a1
ekekalex <ekytoras@gmail.com>
2026-05-02 18:12:10 +1000

    merge fixes and combined things

--- Assets/Scripts/FrameCapTEMP.cs
+++ Assets/Scripts/FrameCapTEMP.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class FrameCapTEMP : MonoBehaviour
 {
+    public int targetFrameRate = 60;
     void Start()
     {
         QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
-        Application.targetFrameRate = 24;
+        Application.targetFrameRate = targetFrameRate;
     }
 
 }
