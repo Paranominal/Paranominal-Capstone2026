03d349c3854bb087db33146ea8509dfbd1dd91f8
lstirling <102574044@student.swin.edu.au>
2026-05-06 04:04:03 +1000

    transferring necessary stuff from enemy-attacks-scoring

---
+++ Assets/Scripts/Scoring/FearBarUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FearBarUI : MonoBehaviour
+{
+    [Header("References")] // can you tell im trying to be better about serializing 
+    [SerializeField] private FearBar fearBar;
+    [SerializeField] private Image barImage;
+    [SerializeField] private TextMeshProUGUI rankText;
+
+    private void Awake()
+    {
+        fearBar.OnFearRankChanged += HandleFearRankChanged;
+        rankText.text = "Fine";
+    }
+
+    private void HandleFearRankChanged(FearBar.FearRank rank)
+    {
+        barImage.fillAmount = Mathf.Clamp01((float)fearBar.FearLevel / fearBar.MaxFear);
+        rankText.text = rank.ToString();
+    }
+}
