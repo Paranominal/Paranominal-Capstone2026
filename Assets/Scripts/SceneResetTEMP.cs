98cf35e25f6e1d356a96288e84294528974aa6d9
ekekalex <ekytoras@gmail.com>
2026-05-07 15:27:29 +1000

    fixed reset input

--- Assets/Scripts/SceneResetTEMP.cs
+++ Assets/Scripts/SceneResetTEMP.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneResetTEMP : MonoBehaviour
 {
+    [SerializeField] private string reset = "Reset";
+    InputAction resetInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         resetInput = InputSystem.actions.FindAction(reset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+        if (resetInput.WasReleasedThisFrame()) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);    }
 }
