9d5f2aea33d3d6d57d6c88a678302a179e878b71
ekekalex <ekytoras@gmail.com>
2026-05-09 15:17:45 +1000

    animation controller updates
    incomplete functionality, but wip

--- Assets/Scripts/EnemyScripts/EnemyAnimationControl.cs
+++ Assets/Scripts/EnemyScripts/EnemyAnimationControl.cs
@@ -8,9 +8,15 @@ public class EnemyAnimationControl : MonoBehaviour
     [SerializeField] private Animator animator;
     //[SerializeField] private EnemyBehaviourBase enemyBehaviour;
     [SerializeField] private EnemyStagger enemyStagger;
+    [Header("Animation Control")]
+    [Tooltip("1 = 4fps")]
+    [SerializeField] private float animSpeed;
+    [SerializeField] private Color stunColour;
+    [SerializeField] private Animation stunAnimation;
 
     void Update()
     {
         animator.SetBool("isStaggered", enemyStagger.IsStaggered);
+        stunAnimation.
     }
 }
