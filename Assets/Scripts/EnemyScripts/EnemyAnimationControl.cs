84f62080cebb9f14b9bd172602c6b260ef7b5942
ekekalex <ekytoras@gmail.com>
2026-05-09 15:02:34 +1000

    removed vestigial code from enemy anim script

--- Assets/Scripts/EnemyScripts/EnemyAnimationControl.cs
+++ Assets/Scripts/EnemyScripts/EnemyAnimationControl.cs
@@ -8,27 +8,9 @@ public class EnemyAnimationControl : MonoBehaviour
     [SerializeField] private Animator animator;
     //[SerializeField] private EnemyBehaviourBase enemyBehaviour;
     [SerializeField] private EnemyStagger enemyStagger;
-    [SerializeField] private WeakPointManager weakPointManager;
-    private bool isStunned = false;
 
     void Update()
     {
         animator.SetBool("isStaggered", enemyStagger.IsStaggered);
-        if (enemyStagger.IsStaggered) ShowWeakness();
-        if (!enemyStagger.IsStaggered) HideWeakness();
-    }
-    public void ShowWeakness()
-    {
-        if (isStunned) return;
-        isStunned = true;
-        weakPointManager.ScanStun();
-        Debug.Log("Did Scan Stun to: " + this);
-    }
-    public void HideWeakness()
-    {
-        if (!isStunned) return;
-        isStunned = false;
-        weakPointManager.HideCurrentWeakpoint();
-        Debug.Log(this + "'s Stun wore off!");
     }
 }
