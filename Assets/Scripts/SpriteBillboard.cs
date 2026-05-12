8e27b2560e4f89b4d0c6f711f2bf12f08413451a
ekekalex <ekytoras@gmail.com>
2026-05-12 13:11:40 +1000

    animations cherry pick cleanup

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -1,14 +1,9 @@
-using System;
 using UnityEngine;
-using UnityEngine.ProBuilder.MeshOperations;
 
 public class SpriteBillboard : MonoBehaviour
 {
-    [Header("Billboard Rotations")] 
-    [SerializeField] private bool rotateX = false;
     void Update()
     {
-        if (!rotateX) transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
-        else transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
