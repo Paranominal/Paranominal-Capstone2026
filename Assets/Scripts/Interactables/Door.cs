989805d3ee0cd1272c115852a83779826a4a70fe
Eleanor Slade <163821768+sladeel@users.noreply.github.com>
2026-05-22 13:02:58 +1000

    updated doors
    the horrors of the stupid font bullshit continue

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -14,21 +14,30 @@ public class Door : MonoBehaviour, IInteractable
     InputAction collectAction;  // this could be rebound to a different action if you prefer
     public bool unlocked;   // at the moment the door can theoretically be "locked" open but thats neither here nor there
     public doorState state;
-    public float speed = 5;
+    public float speed = 10;
     private Quaternion targetRotation;
     public float openAngle = -90;
     public float ajarAngle = -20;
     public float closedAngle = 0;
     public Collider doorCollider;
+    private Quaternion startAngle;
+    public bool isArenaLocked;
+    public float ajarDistance = 3;
+    private PlayerMover player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         collectAction = InputSystem.actions.FindAction("Collect");
         targetRotation = transform.rotation;
+        startAngle = transform.rotation;
         if (doorCollider == null)
         {
             doorCollider = GetComponentInChildren<Collider>();
+        }
+        if (player == null)
+        {
+            player = FindAnyObjectByType<PlayerMover>();    //there is no failsafe here if there's more than one playermover in the scene. don't fuck this up.
         }
     }
 
@@ -42,12 +51,12 @@ public class Door : MonoBehaviour, IInteractable
             {
                 if (state != doorState.open)
                 {
-                    targetRotation = Quaternion.AngleAxis(openAngle, transform.up);
+                    targetRotation = startAngle * Quaternion.AngleAxis(openAngle, transform.up);
                     state = doorState.open;
                 }
                 else
                 {
-                    targetRotation = Quaternion.AngleAxis(ajarAngle, transform.up);
+                    targetRotation = startAngle * Quaternion.AngleAxis(ajarAngle, transform.up);
                     state = doorState.ajar;
                 }
 
@@ -62,6 +71,12 @@ public class Door : MonoBehaviour, IInteractable
         else if (transform.rotation == targetRotation && !doorCollider.enabled)
         {
             doorCollider.enabled = true;
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) > ajarDistance && state == doorState.open)
+        {
+            targetRotation = startAngle * Quaternion.AngleAxis(ajarAngle, transform.up);
+            state = doorState.ajar;
         }
     }
 
@@ -82,11 +97,44 @@ public class Door : MonoBehaviour, IInteractable
 
     public void Close(bool locked)  //locked bool determines if door locks on close
     {
-        targetRotation = Quaternion.AngleAxis(closedAngle, transform.up);
+        targetRotation = startAngle * Quaternion.AngleAxis(closedAngle, transform.up);
         state = doorState.closed;
         if (locked)
         {
             unlocked = false;
+        }
+    }
+
+    public void Unlock()
+    {
+        Interact();
+    }
+
+    public void StartArena()
+    {
+        if (!isArenaLocked)
+        {
+            Close();
+            isArenaLocked = true;
+            Debug.Log("Door locked in Arena mode.");
+        }
+        else
+        {
+            Debug.LogWarning("Door already in Arena mode. Did you mean EndArena()?");
+        }
+    }
+
+    public void EndArena()
+    {
+        if (isArenaLocked)
+        {
+            Unlock();
+            isArenaLocked = false;
+            Debug.Log("Arena mode ended.");
+        }
+        else
+        {
+            Debug.LogWarning("Door is not in Arena mode. Did you mean StartArena()?");
         }
     }
 }
