c62679a671b7c26ceed280f005fc4936b456180b
AntKrueger <antkrueger@hotmail.com>
2026-06-01 15:53:37 +1000

    Worked towards alpha demonstration

---
+++ Assets/Scripts/ShotgunInteraction.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ShotgunInteraction : MonoBehaviour
+{
+    private ShotgunCollection currentItem;
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.E) && currentItem != null)
+    //    {
+    //        currentItem.Interact();
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    ShotgunCollection item = other.GetComponent<ShotgunCollection>();
+
+    //    if (item != null)
+    //    {
+    //        currentItem = item;
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    ShotgunCollection item = other.GetComponent<ShotgunCollection>();
+
+    //    if (item == currentItem)
+    //    {
+    //        currentItem = null;
+    //    }
+    //}
+}
