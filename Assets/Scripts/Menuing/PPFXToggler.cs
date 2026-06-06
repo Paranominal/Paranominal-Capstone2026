40062a4edaa20498dbbe37b8eef2de6081d4fd51
Seth Kalantzis <103992935@student.swin.edu.au>
2026-06-06 22:11:45 +1000

    Added post processing effects toggle

--- Assets/Scripts/Menuing/PPFXToggler.cs
+++ Assets/Scripts/Menuing/PPFXToggler.cs
@@ -1,16 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AdaptivePerformance.Provider;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
-public class TogglePPFX : MonoBehaviour
+
+public class PPFXToggler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private UniversalRendererData PC_Renderer;
+    [SerializeField] private Toggle toggle;
+
+    [SerializeField]
+    private List<string> featureNames = new();
+
+    private readonly List<ScriptableRendererFeature> features = new();
+
+    private void Start()
     {
-        
+        foreach (var rendererFeature in PC_Renderer.rendererFeatures)
+        {
+            if (featureNames.Contains(rendererFeature.name))
+            {
+                features.Add(rendererFeature);
+            }
+        }
+
+        // Initialize toggle from first feature
+        if (features.Count > 0)
+        {
+            toggle.isOn = features[0].isActive;
+        }
+
+        toggle.onValueChanged.AddListener(SetFeaturesActive);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetFeaturesActive(bool enabled)
     {
-        
+        foreach (var feature in features)
+        {
+            feature.SetActive(enabled);
+        }
     }
+
+    private void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(SetFeaturesActive);
+    }
+
 }
