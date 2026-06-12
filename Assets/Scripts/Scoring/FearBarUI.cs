3b822d9bbb45e7197713e99ef7fb21779d769b9c
lstirling <102574044@student.swin.edu.au>
2026-06-12 10:33:41 +1000

    Updates to Fear system
    Now replacing most of Spirit functionality including damage based on enemy attack, trickling (up) fear, game overs, healing on shots, etc.
    NOTE: any calls to healing spirit should be changed to Fear, and calls to modify fear should use float and not int

--- Assets/Scripts/Scoring/FearBarUI.cs
+++ Assets/Scripts/Scoring/FearBarUI.cs
@@ -6,18 +6,28 @@ public class FearBarUI : MonoBehaviour
 {
     [Header("References")] // can you tell im trying to be better about serializing 
     [SerializeField] private FearBar fearBar;
-    [SerializeField] private Image barImage;
+    [SerializeField] private RectTransform barRect;
     [SerializeField] private TextMeshProUGUI rankText;
+
+    private float fullWidth;
 
     private void Awake()
     {
+        fullWidth = barRect.rect.width;
         fearBar.OnFearChanged += HandleFearChanged;
         rankText.text = "Fine";
+        SetBarWidth(0f);
     }
 
     private void HandleFearChanged(FearBar.FearRank rank)
     {
-        barImage.fillAmount = Mathf.Clamp01((float)fearBar.FearLevel / fearBar.MaxFear);
+        SetBarWidth(fearBar.FearLevel / fearBar.MaxFear);
         rankText.text = rank.ToString();
+    }
+
+    private void SetBarWidth(float fraction)
+    {
+        fraction = Mathf.Clamp01(fraction);
+        barRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, fullWidth * fraction); // bitch this was hard to find
     }
 }
