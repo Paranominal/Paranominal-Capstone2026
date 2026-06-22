b1e553be14950404ad1ca810b8faf5b21af2df72
ekekalex <ekytoras@gmail.com>
2026-06-23 01:38:55 +1000

    fixed up bullet spray and other minor adjustments

--- Assets/Scripts/BulletSpray.cs
+++ Assets/Scripts/BulletSpray.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,34 +9,40 @@ public class BulletSpray : MonoBehaviour
     [SerializeField] private InputActionReference shoot;
     [SerializeField] private GameObject bullet;
     [SerializeField] private float bulletSpeed = 10;
+    public float speedVariability = 0;
     [SerializeField] private int maxBulletCount = 100;
-    public float inaccuracy = 0;
+    public float angleInaccuracy = 0;
     public float width = 0;
+    [SerializeField] private bool randomiseSpin;
     public List<GameObject> bullets; //make this public to bug test, make private after
 
     [SerializeField] private BulletSprayFX fx;
+    public bool debugMode;
 
     void Update()
     {
-        AimDir();
-        Inaccuracy();
         if (shoot.action.IsPressed()) Shoot();
         if (fx != null) fx.DoFX();        
     }
     Quaternion AimDir()
     {
-        return Quaternion.LookRotation(transform.forward + Inaccuracy());
+        Quaternion aimDir = Quaternion.LookRotation(transform.forward + Inaccuracy());
+        if (randomiseSpin) aimDir.eulerAngles = aimDir.eulerAngles + RandomSpin();
+        if (debugMode) Debug.Log($"{this} | AimDir():{aimDir}");
+        return aimDir;
     }
     Vector3 AimPos()
     {        
-        return origin.position + OriginWidth();
+        Vector3 aimPos = origin.position + OriginWidth();
+        if (debugMode) Debug.Log($"{this} | AimPos():{aimPos}");
+        return aimPos;
     }
     void Shoot()
     {
         bullets.Add(InstanceBullet());
         foreach (GameObject x in bullets)
         {
-            x.GetComponent<Bullet>().speed = bulletSpeed;
+            x.GetComponent<Bullet>().speed = bulletSpeed + Random.Range(-speedVariability, speedVariability);
         }
         if (bullets.Count > maxBulletCount)
         {
@@ -51,10 +56,20 @@ public class BulletSpray : MonoBehaviour
     }
     Vector3 Inaccuracy()
     {
-        return new Vector3(Random.Range(-inaccuracy, inaccuracy), Random.Range(-inaccuracy, inaccuracy), Random.Range(-inaccuracy, inaccuracy)) * 0.01f;
+        Vector3 inaccuracy = new Vector3 (Random.Range(-angleInaccuracy, angleInaccuracy), Random.Range(-angleInaccuracy, angleInaccuracy), Random.Range(-angleInaccuracy, angleInaccuracy)) * 0.01f;
+        if (debugMode) Debug.Log($"{this} | Innaccuracy():{inaccuracy}");
+        return inaccuracy;
+    }
+    Vector3 RandomSpin()
+    {
+        Vector3 randomSpin = new Vector3(0, 0, Random.Range(0, 360));
+        if (debugMode) Debug.Log($"{this} | RandomSpin():{randomSpin}");
+        return randomSpin;
     }
     Vector3 OriginWidth()
     {
-        return new Vector3(Random.Range(-width, width), Random.Range(-width, width), Random.Range(-width, width)) * 0.01f;
+        Vector3 originWidth = new Vector3 (Random.Range(-width, width), Random.Range(-width, width), Random.Range(-width, width)) * 0.01f;
+        if (debugMode) Debug.Log($"{this} | OriginWidth():{originWidth}");
+        return originWidth;
     }
 }
--- Assets/Scripts/BulletSprayFX.cs
+++ Assets/Scripts/BulletSprayFX.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,14 +8,41 @@ public class BulletSprayFX : MonoBehaviour
 {
     [SerializeField] private InputActionReference shoot;
     [SerializeField] private List<SpriteRenderer> sprites;
-    [SerializeField] private float maxAlpha;
-    [SerializeField] private float minAlpha;
-    public void DoFX()
+    [SerializeField] private List<Light> lights;
+    [SerializeField] private float spriteFadePercent;
+    [SerializeField] private float lightFadePercent;
+    [SerializeField]private List<float> maxAlpha;
+    [SerializeField]private List<float> minAlpha;
+    [SerializeField]private List<float> maxBrightness;
+    [SerializeField]private List<float> minBrightness;
+
+    void Start()
     {
+        //cache sprite and light values
+        foreach (Light light in lights)
+        {
+            maxBrightness.Add(light.intensity);
+            minBrightness.Add(light.intensity * lightFadePercent);
+        }
         foreach (SpriteRenderer sprite in sprites)
         {
-            if (shoot.action.IsPressed()) sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, maxAlpha);
-            else sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, minAlpha);
+            maxAlpha.Add(sprite.color.a);
+            minAlpha.Add(sprite.color.a * spriteFadePercent);
         }
+    }
+    public void DoFX()
+    {
+        int i = 0;
+        foreach (Light light in lights)
+        {
+            if (shoot.action.IsPressed()) light.intensity = maxBrightness[i];
+            else light.intensity = minBrightness[i];
+        }
+        foreach (SpriteRenderer sprite in sprites)
+        {
+            if (shoot.action.IsPressed()) sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, maxAlpha[i]);
+            else sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, minAlpha[i]);
+        }
+        i++;
     }
 }
