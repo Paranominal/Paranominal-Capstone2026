2f27d3b406c431cfdde80e8c272bbe7ae0a0e273
ekekalex <ekytoras@gmail.com>
2026-07-25 02:15:28 +1000

    smooth sprite billboard functionality

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
 
@@ -6,7 +7,33 @@ public class SpriteBillboard : MonoBehaviour
 {
     [Header("Billboard Rotations")] 
     [SerializeField] private bool rotateX = false;
+    [Header("Preferences")] 
+    [SerializeField] private bool slowBillboard = false;
+    [SerializeField] private float slowBillboardTime = 1;
     void Update()
+    {
+        if (slowBillboard) DoSlow();
+        else DoImmediate();
+    }
+
+    Vector3 currentSmoothVelocity;
+    Quaternion targetRotation;
+    void DoSlow()
+    {
+        if (!rotateX) targetRotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        else targetRotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        Vector3 smoothedRotation = Vector3.SmoothDamp
+        (
+            transform.rotation.eulerAngles,
+            targetRotation.eulerAngles,
+            ref currentSmoothVelocity,
+            slowBillboardTime
+        );
+        Quaternion newRotation = Quaternion.Euler(smoothedRotation);
+        transform.rotation = newRotation;
+    }
+
+    void DoImmediate()
     {
         if (!rotateX) transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
         else transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
