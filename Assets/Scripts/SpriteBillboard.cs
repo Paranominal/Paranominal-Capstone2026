e91e66f5e2440a2588439814744da942d41c1c83
ekekalex <ekytoras@gmail.com>
2026-08-10 15:15:41 +1000

    sprite billboard now uses lerps

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -9,7 +9,7 @@ public class SpriteBillboard : MonoBehaviour
     [SerializeField] private bool rotateX = false;
     [Header("Preferences")] 
     [SerializeField] private bool slowBillboard = false;
-    [SerializeField] private float slowBillboardTime = 1;
+    [SerializeField] private float slowBillboardTime = 0.1f;
     void Update()
     {
         if (slowBillboard) DoSlow();
@@ -22,13 +22,7 @@ public class SpriteBillboard : MonoBehaviour
     {
         if (!rotateX) targetRotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
         else targetRotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
-        Vector3 smoothedRotation = Vector3.SmoothDamp
-        (
-            transform.rotation.eulerAngles,
-            targetRotation.eulerAngles,
-            ref currentSmoothVelocity,
-            slowBillboardTime
-        );
+        Vector3 smoothedRotation = Vector3.Lerp(transform.rotation.eulerAngles, targetRotation.eulerAngles, slowBillboardTime);
         Quaternion newRotation = Quaternion.Euler(smoothedRotation);
         transform.rotation = newRotation;
     }
