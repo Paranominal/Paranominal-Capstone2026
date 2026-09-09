a4d975e0facfc6e26f4de13f234686c780f1a0b9
Seth Kalantzis <103992935@student.swin.edu.au>
2026-09-09 17:19:29 +1000

    Revert "Merge pull request #66 from Paranominal/cc-movement-upgrade"
    This reverts commit 3ae4d4d545dad2e51caf7cf124ea4d38e8d1381e, reversing
    changes made to 2a6070a91c72340b11aeb31ebe927f1a42366b96.

--- Assets/Scripts/PlayerScripts/PlayerController/PlayerInputReader.cs
+++ Assets/Scripts/PlayerScripts/PlayerController/PlayerInputReader.cs
@@ -8,8 +8,6 @@ public class PlayerInputReader : MonoBehaviour
     [SerializeField] private InputActionReference moveAction;
     [SerializeField] private InputActionReference sprintAction;
     [SerializeField] private InputActionReference slowWalkAction;
-    [SerializeField] private InputActionReference jumpAction;
-    [SerializeField] private InputActionReference dashAction;
     [SerializeField] private InputActionReference lookActionMouse;
     [SerializeField] private InputActionReference lookActionGamepad;
     [SerializeField] private float gamepadLookSens = 100f;
@@ -29,13 +27,6 @@ public class PlayerInputReader : MonoBehaviour
         : false;
     public bool SlowWalkInput => slowWalkAction != null && slowWalkAction.action != null
         ? slowWalkAction.action.IsPressed()
-        : false;
-    public bool jumpInput => jumpAction != null && jumpAction.action != null
-        ? jumpAction.action.IsPressed()
-        : false;
-
-    public bool dashInput => dashAction != null && dashAction.action != null
-        ? dashAction.action.IsPressed()
         : false;
 
     // public Vector2 LookInput => lookActionMouse != null && lookActionMouse.action != null
@@ -74,9 +65,7 @@ public class PlayerInputReader : MonoBehaviour
         if (moveAction.action.IsPressed()) return true;
         else if (sprintAction.action.IsPressed()) return true;
         else if (slowWalkAction.action.IsPressed()) return true;
-        else if (jumpAction.action.IsPressed()) return true;
         else if (lookActionMouse.action.IsPressed()) return true;
-        else if (dashAction != null && dashAction.action != null && dashAction.action.IsPressed()) return true;
         else return false;
     }
     void DoDebug()
