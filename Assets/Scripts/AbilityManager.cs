fd49d280a59b547d6464e2469a994a1500c27459
Seth Kalantzis <103992935@student.swin.edu.au>
2026-09-22 14:08:07 +1000

    Updated dash bar to hide when dash is disabled, also made small changes to ability manager to align with dash changes

--- Assets/Scripts/AbilityManager.cs
+++ Assets/Scripts/AbilityManager.cs
@@ -5,6 +5,8 @@ public class AbilityManager : MonoBehaviour
     [SerializeField] private ALTGrimoire grimoire;
     [SerializeField] private PlayerDash playerDash;
     [SerializeField] private string dashEntryName;
+    [Tooltip("The version of the dash ability to enable when obtained. Use 'charges' or 'cooldown'.")]
+    [SerializeField] private string dashVersion;
     private bool dashObtained;
 
     void Update()
@@ -15,7 +17,7 @@ public class AbilityManager : MonoBehaviour
     void GainDash()
     {
         playerDash.dashEnabled = true;
-        playerDash.DashVersionEnabled("charges");
+        playerDash.DashVersionEnabled(dashVersion);
         dashObtained = true;
     }
 }
